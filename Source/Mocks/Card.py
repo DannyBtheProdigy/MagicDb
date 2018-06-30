df13f27c566e505bb352592b57d31874530cbe96
DannyBtheProdigy <danbosscher@gmail.com>
2018-06-29 22:44:34 -0400

    Initial Mocks Commit

---
+++ Source/Mocks/Card.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 29, 2018
+
+@author: DannyB
+'''
+
+class Card:
+    def __init__(self):
+        return
+
+
+
+    def LoadImage(self):
+        return
+
+
+    def RemoveImage(self):
+        return
